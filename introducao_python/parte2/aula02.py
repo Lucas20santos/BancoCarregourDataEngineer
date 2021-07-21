4b1e98000e63f7f61d7c8ae307a9faeab3099159
Lucas20santos <lucass.santoss210@gmail.com>
2021-07-21 00:37:35 -0300

    Create aula02.py
    ok

---
+++ introducao_python/parte2/aula02.py
@@ -0,0 +1,16 @@
+a = 10
+b = 5
+
+soma = a + b 
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b 
+resto = a % b 
+
+# print("soma: ", soma)
+# print("subtracao: ", subtracao)
+# print("produto", multiplicacao)
+# print("divisao: ", divisao)
+# print("resto: ", resto)
+
+print(f"soma: {soma}, subtração: {subtracao}, produto: {multiplicacao}, divisao: {divisao}")
