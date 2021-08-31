f5ab5858ebfe02d5f30903a5feebf26443857aff
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2021-08-31 19:46:39 -0300

    final da aula 07

---
+++ introducao_python/aula07/aula07.py
@@ -0,0 +1,27 @@
+## no python os métodos são chamados 
+# de definição, e pode ser chamado da 
+# seguinte maneira:
+
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.a = num1
+        self.b = num2
+    
+    def soma(self):
+        return self.a + self.b
+
+    def subtracao(self):
+        return self.a - self.b
+
+    def multiplicao(self):
+        return self.a * self.b
+
+    def divisao(self):
+        if self.b != 0:
+            return self.a / self.b
+        return -1
+
+calculadora = Calculadora(5, 4)
+
+print(calculadora.soma())
+print(calculadora.subtracao())
