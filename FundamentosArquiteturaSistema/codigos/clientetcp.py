bfb0ed34203dbc658e6820a4ead3497759d66a0b
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2021-09-07 16:02:52 -0300

    final da segunda parte da aula 02

--- FundamentosArquiteturaSistema/codigos/clientetcp.py
+++ FundamentosArquiteturaSistema/codigos/clientetcp.py
@@ -1,3 +1,32 @@
 import socket
+import sys
+import os
+
+os.system("clear")
+
+def main():
+    try:
+        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as erro:
+        print(f"Falha na criação do socket: {erro}")
+        sys.exit()
+    else:
+        print("Socket criado com sucesso!!!")
 
 
+    hostAlvo = input("Digite o hoste ou ip: ")
+    portAlvo = input("Informe a porta para conecção: ")
+
+
+    try:
+        con.connect((hostAlvo, int(portAlvo)))
+        con.shutdown(2)
+    except socket.error as e:
+        print(f"erro na conecção: {e}")
+        sys.exit()
+    else:
+        print("Sucesso!!!")
+        
+
+if __name__ == "__main__":
+    main()
