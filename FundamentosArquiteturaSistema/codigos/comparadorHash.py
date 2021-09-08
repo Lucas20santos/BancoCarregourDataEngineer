949914bd68726e7a1e3dbabd4f47e6be9f014571
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2021-09-07 21:38:51 -0300

    final da terceira parte da aula 03

---
+++ FundamentosArquiteturaSistema/codigos/comparadorHash.py
@@ -0,0 +1,19 @@
+import hashlib
+from os import system as linux
+
+linux("clear")
+
+arquivo1 = "arquivo3.txt"
+arquivo2 = "arquivo4.txt"
+
+hash1 = hashlib.new('ripemd160')
+hash1.update(open(arquivo1, 'rb').read())
+
+
+hash2 = hashlib.new('ripemd160')
+hash2.update(open(arquivo2, 'rb').read())
+
+if hash1.digest()!= hash2.digest():
+    print("Os arquivos são diferentes")
+else:
+    print("Os arquivos são iguais")
